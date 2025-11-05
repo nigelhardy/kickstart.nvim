1db32caeda2fae64e13eeae86dc1b32cc367bdaa
Nigel Hardy <nigel@heronpower.com>
2025-11-05 10:57:52 -0800

    Hopefully fixed the git parser issues

--- init.lua
+++ init.lua
@@ -1105,7 +1105,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'powershell', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'gitcommit', 'git_rebase', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'powershell', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {

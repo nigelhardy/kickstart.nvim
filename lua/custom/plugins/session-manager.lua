bda5a908a04953041b31380c0a841f0500018d93
Nigel Hardy <nigeljames.12@gmail.com>
2025-11-07 22:38:40 -0800

    Load session based on location/git repo not last session

--- lua/custom/plugins/session-manager.lua
+++ lua/custom/plugins/session-manager.lua
@@ -9,7 +9,7 @@ return {
     
     require('session_manager').setup({
       sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
-      autoload_mode = config.AutoloadMode.LastSession, -- Define what to do when Neovim is started without arguments.
+      autoload_mode = config.AutoloadMode.GitSession, -- Define what to do when Neovim is started without arguments.
       autosave_last_session = true, -- Automatically save last session on exit and on session switch.
       autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
       autosave_ignore_dirs = {}, -- A list of directories where the session will not be autosaved.

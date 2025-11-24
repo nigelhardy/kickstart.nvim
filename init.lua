6ead68e04d04cedb6fbb1bc8faca2afba26e7624
Nigel Hardy <nigeljames.12@gmail.com>
2025-11-24 11:26:10 -0800

    Updated to consolidate lsp code and removed top comment from kickstart

--- init.lua
+++ init.lua
@@ -1,87 +1,4 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-===================================================================== ========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
+-- Nigel's Neovim Configuration
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
@@ -96,9 +13,6 @@ end
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
-
-vim.lsp.enable 'pyright'
-vim.lsp.enable 'ast_grep'
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -187,6 +101,7 @@ vim.o.smartindent = true -- Smart autoindenting on new lines
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- TODO move this little c/h helper, maybe only active on .c and .h files somehow?
 vim.keymap.set('n', '<leader>a', function()
   local ext = vim.fn.expand '%:e'
   local base = vim.fn.expand '%:r'
@@ -208,9 +123,9 @@ vim.keymap.set('n', '<leader>cd', ':cd %:p:h<CR>:pwd<CR>', { desc = 'CD to curre
 -- Global clangd LSP toggle (works even when no LSP attached)
 vim.keymap.set('n', '<leader>tc', function()
   local buf = vim.api.nvim_get_current_buf()
-  local clients = vim.lsp.get_clients({ bufnr = buf })
+  local clients = vim.lsp.get_clients { bufnr = buf }
   local clangd_client = nil
-  
+
   -- Find clangd client for this buffer
   for _, client in ipairs(clients) do
     if client.name == 'clangd' then
@@ -218,7 +133,7 @@ vim.keymap.set('n', '<leader>tc', function()
       break
     end
   end
-  
+
   if clangd_client then
     -- Stop clangd for this buffer
     vim.lsp.stop_client(clangd_client.id)
@@ -227,7 +142,7 @@ vim.keymap.set('n', '<leader>tc', function()
     -- Start clangd for this buffer
     local filetype = vim.bo[buf].filetype
     if vim.tbl_contains({ 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' }, filetype) then
-      vim.lsp.enable('clangd')
+      vim.lsp.enable 'clangd'
       vim.notify('Clangd LSP enabled for buffer', vim.log.levels.INFO)
     else
       vim.notify('Clangd not available for filetype: ' .. filetype, vim.log.levels.WARN)
@@ -693,9 +608,9 @@ require('lazy').setup({
           -- Toggle clangd LSP on/off for current buffer
           map('<leader>tc', function()
             local buf = event.buf
-            local clients = vim.lsp.get_clients({ bufnr = buf })
+            local clients = vim.lsp.get_clients { bufnr = buf }
             local clangd_client = nil
-            
+
             -- Find clangd client for this buffer
             for _, client in ipairs(clients) do
               if client.name == 'clangd' then
@@ -703,7 +618,7 @@ require('lazy').setup({
                 break
               end
             end
-            
+
             if clangd_client then
               -- Stop clangd for this buffer
               vim.lsp.stop_client(clangd_client.id)
@@ -712,7 +627,7 @@ require('lazy').setup({
               -- Start clangd for this buffer
               local filetype = vim.bo[buf].filetype
               if vim.tbl_contains({ 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' }, filetype) then
-                vim.lsp.enable('clangd')
+                vim.lsp.enable 'clangd'
                 vim.notify('Clangd LSP enabled for buffer', vim.log.levels.INFO)
               else
                 vim.notify('Clangd not available for filetype: ' .. filetype, vim.log.levels.WARN)
@@ -786,7 +701,8 @@ require('lazy').setup({
           },
         },
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        ast_grep = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --

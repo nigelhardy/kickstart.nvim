c241ae951279e444bd6e1b2379b8b15765d1db06
Nigel Hardy <nigeljames.12@gmail.com>
2025-11-25 15:29:38 -0800

    Simplified theme situation into one place

--- init.lua
+++ init.lua
@@ -749,35 +749,7 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load colorscheme immediately in init to avoid startup delay
-      vim.cmd.colorscheme 'tokyonight-night'
 
-      -- Make background transparent
-      vim.cmd [[
-        highlight Normal guibg=NONE ctermbg=NONE
-        highlight NormalNC guibg=NONE ctermbg=NONE
-        highlight SignColumn guibg=NONE ctermbg=NONE
-        highlight EndOfBuffer guibg=NONE ctermbg=NONE
-      ]]
-    end,
-    opts = {
-      transparent = true,
-      styles = {
-        comments = { italic = false }, -- Disable italics in comments
-        sidebars = 'transparent',
-        floats = 'transparent',
-      },
-    },
-  },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,34 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  -- Primary colorscheme
+  {
+    'folke/tokyonight.nvim',
+    lazy = false, -- Load at startup to avoid flash
+    priority = 1000, -- Highest priority to load first
+    init = function()
+      -- Load colorscheme immediately to avoid startup delay
+      vim.cmd.colorscheme 'tokyonight-night'
+
+      -- Make background transparent
+      vim.cmd [[
+        highlight Normal guibg=NONE ctermbg=NONE
+        highlight NormalNC guibg=NONE ctermbg=NONE
+        highlight SignColumn guibg=NONE ctermbg=NONE
+        highlight EndOfBuffer guibg=NONE ctermbg=NONE
+      ]]
+    end,
+    opts = {
+      transparent = true,
+      styles = {
+        comments = { italic = false }, -- Disable italics in comments
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
+  },
+
+  -- Alternative colorscheme (lazy loaded)
   {
     'ellisonleao/gruvbox.nvim',
     priority = 1000,
